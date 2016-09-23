c6fd686eb0461fa29dd31bd9383c9aa88ff21e68
Charles Crawford <charliec364@gmail.com>
2016-09-23 03:05:21 -0700

    update

--- 1-buffers/labs/arrayStream.js
+++ 1-buffers/labs/arrayStream.js
@@ -1,4 +1,5 @@
 var Readable = require('stream').Readable
+var Writable = require('stream').Writable
 
 class ArrayStreamer extends Readable {
   constructor(array, options) {
@@ -16,4 +17,21 @@ class ArrayStreamer extends Readable {
 }
 
 var arrayStream = new ArrayStreamer([2, 5, 10, 12])
-arrayStream.pipe(process.stdout)
+//arrayStream.pipe(process.stdout)
+
+class ArrayStreamWriter extends Writable {
+  constructor(options) {
+    super(options)
+    this.sink = []
+  }
+
+  _write(chunk, encoding, callback) {
+    this.sink.push(chunk)
+    callback()
+  }
+}
+
+var arrayWriter = new ArrayStreamWriter()
+arrayStream.pipe(arrayWriter)
+
+console.log(arrayWriter.sink)
